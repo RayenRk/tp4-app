4bba07e6004cbf37c3cfd3b04256f7db10c679f5
RayenRk <rakkadrayen@gmail.com>
2024-05-16 15:53:53 +0100

    navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
